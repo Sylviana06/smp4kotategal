800d3c0e911059e4fd164fff9483d557143044c8
pangestutauhid10 <pangestutauhid1994@gmail.com>
2021-02-08 14:23:27 +0700

    halaman1

--- app/src/main/java/com/example/smpn4kotategal/MainActivity.java
+++ app/src/main/java/com/example/smpn4kotategal/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.smpn4kotategal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void profil(View view){
+        Intent intent=new Intent(MainActivity.this, ProfilActivity.class);
+        startActivity(intent);
+    }
+
+    public void lokasi(View view){
+        Intent intent=new Intent(MainActivity.this,LokasiActivity.class);
+        startActivity(intent);
+    }
+
+    public void prestasi(View view){
+        Intent intent=new Intent(MainActivity.this,PrestasiActivity.class);
+        startActivity(intent);
+    }
+
+    public void ekstakulikuler(View view){
+        Intent intent=new Intent(MainActivity.this, EkstrakulikulerActivity.class);
+        startActivity(intent);
+    }
+
+    public void hubungi(View view){
+        Intent intent=new Intent(MainActivity.this, HubungiActivity.class);
+        startActivity(intent);
+    }
+
+    public void download(View view){
+        Intent intent=new Intent(MainActivity.this, DownloadActivity.class);
+        startActivity(intent);
     }
 }
