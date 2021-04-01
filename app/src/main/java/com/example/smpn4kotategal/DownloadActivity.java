cb61a9873c43f82c3285de2955e12ef7fc3f6d1e
pangestutauhid10 <pangestutauhid1005@gmail.com>
2021-04-01 13:02:10 +0700

    profil

--- app/src/main/java/com/example/smpn4kotategal/DownloadActivity.java
+++ app/src/main/java/com/example/smpn4kotategal/DownloadActivity.java
@@ -2,7 +2,9 @@ package com.example.smpn4kotategal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DownloadActivity extends AppCompatActivity {
 
@@ -11,4 +13,26 @@ public class DownloadActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
     }
+    public void album1(View view){
+        Intent intent=new Intent(DownloadActivity.this, album1Activity.class);
+        startActivity(intent);
+    }
+    public void album2(View view){
+        Intent intent=new Intent(DownloadActivity.this, album2Activity.class);
+        startActivity(intent);
+    }
+    public void album3(View view){
+        Intent intent=new Intent(DownloadActivity.this, album3Activity.class);
+        startActivity(intent);
+    }
+    public void album4(View view){
+        Intent intent=new Intent(DownloadActivity.this, album4Activity.class);
+        startActivity(intent);
+    }
+    public void album5(View view){
+        Intent intent=new Intent(DownloadActivity.this, album5Activity.class);
+        startActivity(intent);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/smpn4kotategal/album3Activity.java
@@ -0,0 +1,14 @@
+package com.example.smpn4kotategal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class album3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album3);
+    }
+}
